c6fc285d014b62e75d91a0e7b6c12270953cedbf
Botond Osváth <osvathbotond25@gmail.com>
2023-11-07 15:26:09 +0100

    fixed paths in fan_control.sh

--- fan_control.sh
+++ fan_control.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-pid=$(cat ~/fan_control/process_pid)
+BASEDIR=$(dirname "$0")
+pid=$(cat ${BASEDIR}/process_pid)
 
 if kill -0 "$pid" 2>/dev/null; then
     kill $pid
-    echo "" > ~/fan_control/process_pid
+    echo "" > ${BASEDIR}/process_pid
     echo "Stopped the fan control on PID $pid!"
 else
-    GPIOZERO_PIN_FACTORY=rpigpio python3 ~/fan_control/fan_control.py &
+    GPIOZERO_PIN_FACTORY=rpigpio python3 ${BASEDIR}/fan_control.py &
     pid=$!
-    echo $pid > ~/fan_control/process_pid
+    echo $pid > ${BASEDIR}/process_pid
     echo "Started the fan control on PID $pid!"
 fi
